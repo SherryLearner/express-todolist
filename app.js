7754711efd62150c2ec3cdf58557d8e0b60427ac
SherryMaster <128972759+SherryMaster@users.noreply.github.com>
2024-03-22 18:58:18 +0500

    Ability to delete tasks

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 
 tasks = []
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
@@ -20,6 +20,20 @@ app.post('/', (req, res) => {
     tasks.push(item)
     res.redirect('/')
 })
+
+app.post('/delete', (req, res) => {
+    const index = req.body && req.body.index;
+    if (index !== undefined) {
+        tasks.splice(index, 1);
+        res.sendStatus(200); // send success response
+    } else {
+        // Handle the case when 'index' is undefined
+        res.status(400).send('Invalid request');
+        res.sendStatus(400); // send error response
+        res.send('Invalid request');
+    }
+});
+
 app.listen(port, () =>
-    console.log(`Example app listening on port http://localhost:${port}`)
+    console.log(`Example app listening on port ${port}`)
 )
