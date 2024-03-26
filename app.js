ce4408720269219bfbae84c44f26a8c2ba05a01f
SherryMaster <128972759+SherryMaster@users.noreply.github.com>
2024-03-26 06:29:54 +0500

    checking a task now saves it

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.PORT || 3000
 
-tasks = []
+tasks = {
+    "Root": []
+}
 
 app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
@@ -13,28 +15,37 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
     const new_date = new Date()
     date = new_date.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
-    res.render('list', { date: date, tasks: tasks })
+    res.render('list', { title: date, tasks: tasks["Root"] })
 })
 
 app.post('/', (req, res) => {
-    const item = req.body.task
-    tasks.push(item)
+    const task = req.body.task
+    tasks["Root"].push({title: task, description: task, completed: false})
     res.redirect('/')
 })
 
 app.post('/delete', (req, res) => {
-    const index = req.body && req.body.index;
+    const index = req.body.id;
     if (index !== undefined) {
-        tasks.splice(index, 1);
+        tasks["Root"].splice(index, 1);
         res.sendStatus(200); // send success response
     } else {
         // Handle the case when 'index' is undefined
         res.status(400).send('Invalid request');
-        res.sendStatus(400); // send error response
-        res.send('Invalid request');
     }
 });
 
+app.post('/update', (req, res) => {
+    const index = req.body.id;
+    if (index !== undefined) {
+        tasks["Root"][index].completed = !tasks["Root"][index].completed;
+        res.sendStatus(200); // send success response
+    } else {
+        // Handle the case when 'index' is undefined
+        res.status(400).send('Invalid request');
+    }
+})
+
 app.listen(port, () =>
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Todo app listening on port ${port}`)
 )
